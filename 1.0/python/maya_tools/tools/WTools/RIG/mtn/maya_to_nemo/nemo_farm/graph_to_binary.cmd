af97e1f79363983c81e88ccbabd7783a69a0ed0f
yangtao <toddyyoung@foxmail.com>
2023-12-29 11:24:16 +0800

    fix bug, CMD does not support the UNC path as the current directory

--- 1.0/python/maya_tools/tools/WTools/RIG/mtn/maya_to_nemo/nemo_farm/graph_to_binary.cmd
+++ 1.0/python/maya_tools/tools/WTools/RIG/mtn/maya_to_nemo/nemo_farm/graph_to_binary.cmd
@@ -1,6 +1,4 @@
 @echo off
 call Y:\pipeline_studio\config\base_env.bat
 
-cd /d %~dp0
-
-rez-env python-3 requests -- python %cd%\graph_to_binary.py %*
+rez-env python-3 requests -- python %~dp0\graph_to_binary.py %*
