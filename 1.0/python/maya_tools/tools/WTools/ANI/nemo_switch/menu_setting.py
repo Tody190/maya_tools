875783fd18e8764e9e0cd651e86984d0cdadca64
yangtao <toddyyoung@foxmail.com>
2023-12-29 22:04:15 +0800

    add opt

--- 1.0/python/maya_tools/tools/WTools/ANI/nemo_switch/menu_setting.py
+++ 1.0/python/maya_tools/tools/WTools/ANI/nemo_switch/menu_setting.py
@@ -5,9 +5,14 @@
 
 menu_type = "menuItem"  # menu or submenu or menuItem
 
-
 label = u"切换为 Nemo Rig"
 
 
 def command():
     execute('switch_to_nemo.switch()')
+
+
+def option_command():
+    import os
+    help_url = 'https://cg.wlf.com/zh/%E7%8E%AF%E8%8A%82/%E7%BB%91%E5%AE%9A%E5%B7%A5%E5%85%B7/nemo_switchernemo_switcher'
+    os.startfile(help_url)
